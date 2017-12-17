03e72cb3ee3867a801bc3d60be6c3914668f653f
DESKTOP-9PIVI1O\ALong <zkrguojl@abchinalife.cn>
2017-12-17 15:12:54 +0800

    12-17提交

--- app/src/main/java/com/along/image/MainActivity.java
+++ app/src/main/java/com/along/image/MainActivity.java
@@ -8,33 +8,13 @@ import android.os.Bundle;
 
 import com.along.image.utils.OkHttpNetUtils;
 
-public class MainActivity extends AppCompatActivity implements OkHttpNetUtils.RequestCallBack {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OkHttpNetUtils.getStringAsync("",this);
     }
 
-    @Override
-    public void onSucceed(String result) {
-
-    }
-
-    @Override
-    public void onFailure(String message) {
-
-    }
-
-    @Override
-    public void onPrePare() {
-
-    }
-
-    @Override
-    public void onLoadFinish() {
-
-    }
 }
