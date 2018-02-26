87844e87f18073ce02fe13c88ac0e9ee50ee0b52
zkrguojl <zkrguojl@abchinalife.cn>
2018-02-26 17:51:28 +0800

    02-26更新,Rxjava初体验

---
+++ app/src/main/java/com/along/image/test/rxjava/RxjavaTest.java
@@ -0,0 +1,49 @@
+package com.along.image.test.rxjava;
+
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by mip on 2018/2/26.
+ */
+
+public class RxjavaTest {
+
+    public static void RxTransmit(){
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+}
